eafd403fe4b8e5dd5364506bda72bd6a9c481a44
doofenschmirtzz <33329300+doofenschmirtzz@users.noreply.github.com>
2018-06-10 18:08:50 +0700

    update lan 3
    Map1

---
+++ THE WILL TO ACT/Assets/Script/WorldInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour {
+
+    NavMeshAgent playerAgent;
+    private bool run=false;
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        playerAgent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            GetInteraction();
+        }
+	}
+    void GetInteraction()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(ray,out interactionInfo,Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if(interactedObject.tag=="Interactable Object")
+            {
+                Debug.Log("Interactable interacted.");
+            }
+            else
+            {
+                playerAgent.destination = interactionInfo.point;
+                run = true;
+            }
+            
+        }
+        anim.SetBool("IsRunning", run);
+        
+    }
+}
